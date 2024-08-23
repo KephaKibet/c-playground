5b5fe1256bab3d378f731dedeb0996867c2badf3
Kepha Kibet <Kibet.kepha@live.com>
2024-08-23 19:16:44 +0300

    initial commit

---
+++ c#  playground/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c___playground
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int one = 2;
+            int two = 5;
+            var remainder = two % one;
+
+            Console.WriteLine(remainder);
+            //Console.ReadLine();
+        }
+    }
+}
