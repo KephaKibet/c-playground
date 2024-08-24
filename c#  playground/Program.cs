584f8eacdb496cc7c639adca7e3b37b013496083
Kepha Kibet <Kibet.kepha@live.com>
2024-08-24 17:47:35 +0300

    FizzBuzz

--- c#  playground/Program.cs
+++ c#  playground/Program.cs
@@ -1,21 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace c___playground
+namespace FizzBuzz
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int one = 2;
-            int two = 5;
-            var remainder = two % one;
-
-            Console.WriteLine(remainder);
-            //Console.ReadLine();
+           
+            for (int i = 1; i <= 100; ++i) 
+            {
+                if (i % 3 == 0 && i % 5 == 0) 
+                {
+                    Console.Write("FizzBuzz");
+                }
+                else if (i % 3 == 0) 
+                {
+                    Console.Write("Fizz");
+                }
+                else if (i % 5 == 0) 
+                {
+                    Console.Write("Buzz");
+                }
+                 else
+                {
+                    Console.WriteLine(i);
+                }
+            }  
         }
     }
 }
